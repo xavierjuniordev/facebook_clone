9c030b7205bf226f306159931123019e0d7ec10f
xavierjuniordev <xavierjunior82@gmail.com>
2020-12-07 18:05:15 -0300

    Conclusão da ToolBar.js

--- components/ToolBar.js
+++ components/ToolBar.js
@@ -48,9 +48,15 @@ const Separator = styled.View`
   height: 26px;
   background: #f0f0f0;
 `;
+const BottonDivider = styled.View`
+  width: 100%;
+  height: 9px;
+  background: #f0f2f5;
+`
 
 const ToolBar = () => {
   return (
+    <>
     <Container>
       <Row>
         <Avatar source={user5} />
@@ -81,7 +87,9 @@ const ToolBar = () => {
         </Menu>
         <Separator />
       </Row>
-    </Container>
+    </Container>  
+    <BottonDivider/>
+    </> 
   );
 };
 
---
+++ components/Users.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+    width: 100%;
+    height: 58px;
+    flex-direction: row;
+    align-items: center;
+`
+const Room = styled.TouchableOpacity`
+    width: 115px;
+    height: 40px;
+    flex-direction:
+    border-radius: 20px;
+    border-width: 1px;
+    border-color: #82b1ff;
+    padding: 0 15px;
+
+
+`
+
+const Text = styled.Text`
+    color: #1777f2;
+    font-size: 
+`
+const Users = () => {
+    return (
+        
+    )
+}
+    
